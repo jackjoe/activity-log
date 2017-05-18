a83a171c47d6f6773cc07a64f9164ceaa4a65d56
Pieter Michels <pieter@noort.be>
2017-05-18 09:51:57 +0200

    syntax

--- src/ActivityLogServiceProvider.php
+++ src/ActivityLogServiceProvider.php
@@ -3,48 +3,48 @@
 use Illuminate\Support\ServiceProvider;
 
 class ActivityLogServiceProvider extends ServiceProvider {
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+  /**
+   * Indicates if loading of the provider is deferred.
+   *
+   * @var bool
+   */
+  protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->publishes([
-			__DIR__ . '/config/activity-log.php' => config_path('activity-log.php'),
-		]);
+  /**
+   * Bootstrap the application events.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+    $this->publishes([
+      __DIR__ . '/config/activity-log.php' => config_path('activity-log.php'),
+    ]);
 
-		$this->publishes([
-			__DIR__ . '/migrations' => database_path('migrations'),
+    $this->publishes([
+      __DIR__ . '/migrations' => database_path('migrations'),
     ], 'migrations');
 
     $this->mergeConfigFrom(__DIR__ . '/config/activity-log.php', 'activity-log');
-	}
+  }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		//
-	}
+  /**
+   * Register the service provider.
+   *
+   * @return void
+   */
+  public function register()
+  {
+    //
+  }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return [];
-	}
+  /**
+   * Get the services provided by the provider.
+   *
+   * @return array
+   */
+  public function provides()
+  {
+    return [];
+  }
 }
--- src/Models/Activity.php
+++ src/Models/Activity.php
@@ -5,92 +5,92 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Request;
 
 class Activity extends Eloquent {
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
-	protected $table = 'activity_log';
+  /**
+   * The database table used by the model.
+   *
+   * @var string
+   */
+  protected $table = 'activity_log';
 
-	/**
-	 * The fillable fields for the model.
-	 *
-	 * @var    array
-	 */
-	protected $fillable = [
-		'user_id',
-		'content_type',
-		'content_id',
-		'action',
-		'description',
-		'details',
-		'data',
-		'version',
-		'ip_address',
-		'user_agent',
-	];
+  /**
+   * The fillable fields for the model.
+   *
+   * @var    array
+   */
+  protected $fillable = [
+    'user_id',
+    'content_type',
+    'content_id',
+    'action',
+    'description',
+    'details',
+    'data',
+    'version',
+    'ip_address',
+    'user_agent',
+  ];
 
-	/**
-	 * Get the user that the activity belongs to.
-	 *
-	 * @return object
-	 */
-	public function user()
-	{
-		return $this->belongsTo(config('auth.providers.users.model'), 'user_id');
+  /**
+   * Get the user that the activity belongs to.
+   *
+   * @return object
+   */
+  public function user()
+  {
+    return $this->belongsTo(config('auth.providers.users.model'), 'user_id');
   }
 
-	/**
-	 * Create an activity log entry.
-	 *
-	 * @param  mixed    $data
-	 * @return boolean
-	 */
-	public static function log($data = [])
-	{
-		// set the defaults from config
-		$defaults = config('activity-log.defaults');
-    if(!is_array($defaults)) {
-			$defaults = [];
+  /**
+   * Create an activity log entry.
+   *
+   * @param  mixed    $data
+   * @return boolean
+   */
+  public static function log($data = [])
+  {
+    // set the defaults from config
+    $defaults = config('activity-log.defaults');
+    if (!is_array($defaults)) {
+      $defaults = [];
     }
 
-    if(is_object($data)) {
+    if (is_object($data)) {
       $data = (array) $data;
     }
 
     // set the user ID
-		if(config('activity-log.auto_set_user_id') && !isset($data['userId'])) {
+    if (config('activity-log.auto_set_user_id') && !isset($data['userId'])) {
       $user = call_user_func(config('activity-log.auth_method'));
-			$data['userId'] = isset($user->id) ? $user->id : null;
-		}
-
-		// set IP address
-    if(!isset($data['ipAddress'])) {
-			$data['ipAddress'] = Request::getClientIp();
+      $data['userId'] = isset($user->id) ? $user->id : null;
     }
 
-		// set user agent
+    // set IP address
+    if (!isset($data['ipAddress'])) {
+      $data['ipAddress'] = Request::getClientIp();
+    }
+
+    // set user agent
     if (!isset($data['userAgent'])) {
-			$data['userAgent'] = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : 'No User Agent';
+      $data['userAgent'] = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : 'No User Agent';
     }
 
-		// set additional data and encode it as JSON if it is an array or an object
-    if(isset($data['data']) && (is_array($data['data']) || is_object($data['data']))) {
-			$data['data'] = json_encode($data['data']);
+    // set additional data and encode it as JSON if it is an array or an object
+    if (isset($data['data']) && (is_array($data['data']) || is_object($data['data']))) {
+      $data['data'] = json_encode($data['data']);
     }
 
-		// format array keys to snake case for insertion into database
-		$dataFormatted = [];
-		foreach ($data as $key => $value) {
-			$dataFormatted[snake_case($key)] = $value;
-		}
+    // format array keys to snake case for insertion into database
+    $dataFormatted = [];
+    foreach ($data as $key => $value) {
+      $dataFormatted[snake_case($key)] = $value;
+    }
 
-		// merge defaults array with formatted data array
-		$data = array_merge($defaults, $dataFormatted);
+    // merge defaults array with formatted data array
+    $data = array_merge($defaults, $dataFormatted);
 
-		// create the record
-		$self = static::create($data);
+    // create the record
+    $self = static::create($data);
 
-		return $self;
-	}
+    return $self;
+  }
 }
