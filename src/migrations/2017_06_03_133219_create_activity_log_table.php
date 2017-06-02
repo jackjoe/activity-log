b92ba8c6274f19080c04b24b4c5fd0ba03ba9be3
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2017-06-02 11:40:35 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/migrations/2017_06_03_133219_create_activity_log_table.php
+++ src/migrations/2017_06_03_133219_create_activity_log_table.php
@@ -4,43 +4,43 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 class CreateActivityLogTable extends Migration {
-  /**
-   * Run the migrations.
-   *
-   * @return void
-   */
-  public function up()
-  {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
     Schema::create('activity_log', function(Blueprint $table)
     {
-      $table->increments('id');
+        $table->increments('id');
 
-      $table->integer('user_id')->nullable();
-      $table->integer('content_id')->nullable();
+        $table->integer('user_id')->nullable();
+        $table->integer('content_id')->nullable();
 
-      $table->string('content', 72)->nullable();
-      $table->string('action', 32)->nullable();
-      $table->string('state', 32)->nullable();
+        $table->string('content', 72)->nullable();
+        $table->string('action', 32)->nullable();
+        $table->string('state', 32)->nullable();
 
-      $table->longText('details')->nullable();
-      $table->longText('data')->nullable();
+        $table->longText('details')->nullable();
+        $table->longText('data')->nullable();
 
-      $table->string('version', 10)->nullable();
-      $table->string('ip_address', 64);
-      $table->string('user_agent', 255);
+        $table->string('version', 10)->nullable();
+        $table->string('ip_address', 64);
+        $table->string('user_agent', 255);
 
-      $table->timestamp('created_at')->useCurrent();
-      $table->timestamp('updated_at')->useCurrent();
+        $table->timestamp('created_at')->useCurrent();
+        $table->timestamp('updated_at')->useCurrent();
     });
-  }
+    }
 
-  /**
-   * Reverse the migrations.
-   *
-   * @return void
-   */
-  public function down()
-  {
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
     Schema::drop('activity_log');
-  }
+    }
 }
